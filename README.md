95d041135968c732ce35df34481ef831d0d4a554
Aidan Timson <contact@timmo.xyz>
2023-10-22 19:15:14 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# System Bridge - CLI
+
+This is a CLI interface package for the [System Bridge](https://github.com/timmo001/system-bridge) project.
--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ with io.open("requirements.txt", encoding="utf-8") as f:
 setup(
     name="systembridgeconnector",
     description="System Bridge Connector",
-    keywords="system bridge connector",
+    keywords="system-bridge",
     author="Aidan Timson (Timmo)",
     author_email="aidan@timmo.dev",
     license="MIT",
-    url="https://github.com/timmo001/system-bridge",
+    url="https://github.com/timmo001/system-bridge-connector",
     packages=find_packages(exclude=["tests", "generator"]),
     install_requires=requirements,
     setup_requires=requirements_setup,
