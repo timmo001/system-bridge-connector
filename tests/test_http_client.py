40fc47c08e18e082d7fa5221ae7face0c0068039
Aidan Timson <aidan@timmo.dev>
2024-03-06 00:07:52 +0000

    Refactor HTTP client tests to include JSON and text responses

--- tests/test_http_client.py
+++ tests/test_http_client.py
@@ -8,18 +8,24 @@ from systembridgeconnector.http_client import HTTPClient
 from . import API_HOST, API_PORT, TOKEN, ClientSessionGenerator
 
 
-async def _test_response(_: web.Request):
-    """Return a test response."""
+async def _json_response(_: web.Request):
+    """Return a json response."""
     return web.json_response({"test": "test"})
+
+
+async def _text_response(_: web.Request):
+    """Return a text response."""
+    return web.Response(text="test")
 
 
 async def _get_http_client(aiohttp_client: ClientSessionGenerator) -> HTTPClient:
     """Return a HTTP client."""
     app = web.Application()
-    app.router.add_delete("/", _test_response)
-    app.router.add_get("/", _test_response)
-    app.router.add_post("/", _test_response)
-    app.router.add_put("/", _test_response)
+    app.router.add_delete("/test/json", _json_response)
+    app.router.add_get("/test/json", _json_response)
+    app.router.add_get("/test/text", _text_response)
+    app.router.add_post("/test/json", _json_response)
+    app.router.add_put("/test/json", _json_response)
 
     client = await aiohttp_client(
         app,
@@ -40,29 +46,36 @@ async def _get_http_client(aiohttp_client: ClientSessionGenerator) -> HTTPClient
 async def test_delete(aiohttp_client: ClientSessionGenerator):
     """Test the delete method."""
     client = await _get_http_client(aiohttp_client)
-    response = await client.delete("/", None)
-    assert response == {"test": "test"}
+    response_json = await client.delete("/test/json", None)
+    assert isinstance(response_json, dict)
+    assert response_json == {"test": "test"}
 
 
 @pytest.mark.asyncio
 async def test_get(aiohttp_client: ClientSessionGenerator):
     """Test the get method."""
     client = await _get_http_client(aiohttp_client)
-    response = await client.get("/")
-    assert response == {"test": "test"}
+    response_json = await client.get("/test/json")
+    assert isinstance(response_json, dict)
+    assert response_json == {"test": "test"}
+
+    response_text = await client.get("/test/text")
+    assert response_text == "test"
 
 
 @pytest.mark.asyncio
 async def test_post(aiohttp_client: ClientSessionGenerator):
     """Test the post method."""
     client = await _get_http_client(aiohttp_client)
-    response = await client.post("/", None)
-    assert response == {"test": "test"}
+    response_json = await client.post("/test/json", None)
+    assert isinstance(response_json, dict)
+    assert response_json == {"test": "test"}
 
 
 @pytest.mark.asyncio
 async def test_put(aiohttp_client: ClientSessionGenerator):
     """Test the put method."""
     client = await _get_http_client(aiohttp_client)
-    response = await client.put("/", None)
-    assert response == {"test": "test"}
+    response_json = await client.put("/test/json", None)
+    assert isinstance(response_json, dict)
+    assert response_json == {"test": "test"}
