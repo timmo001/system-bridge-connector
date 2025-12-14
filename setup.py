4cf877c1667a6acf055fdc885d194ae635316b12
timmo001 <28114703+timmo001@users.noreply.github.com>
2025-12-14 02:51:13 +0000

    Bump systembridgeconnector version to 5.2.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="systembridgeconnector",
-    version="5.2.3.dev0",
+    version="5.2.3",
     author="Aidan Timson (Timmo)",
     author_email="aidan@timmo.dev",
     description="System Bridge Connector",
