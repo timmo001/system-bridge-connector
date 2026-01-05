35082d6aa3b8ef6f7385b770e8df38308dcd68e3
Aidan Timson <aidan@timmo.dev>
2026-01-05 19:01:20 +0000

    Ignore

--- systembridgeconnector/models/settings.py
+++ systembridgeconnector/models/settings.py
@@ -86,7 +86,7 @@ class SettingsCommandDefinition:
     id: str
     name: str
     command: str
-    workingDir: str = ""  # noqa: N815
+    workingDir: str = ""  # noqa: N815  # pylint: disable=C0103
     arguments: list[str] = field(default_factory=lambda: [])
 
 
